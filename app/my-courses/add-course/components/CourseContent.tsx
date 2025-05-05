8610787cb3fbf5c4e63667e980b3eca74e179621
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-05-05 13:07:58 +0600

    feat: Simplify CourseContent component by removing unused HTML handling and localStorage logic; add Tailwind typography dependency

--- app/my-courses/add-course/components/CourseContent.tsx
+++ app/my-courses/add-course/components/CourseContent.tsx
@@ -7,29 +7,33 @@ import "@/components/tiptap-node/list-node/list-node.scss"
 import "./style.css"
 
 export default function CourseContent({ course, setCourse }: any) {
-    const savedHtml = JSON.parse(localStorage.getItem("TextEditorContent") || "")
-    console.log(savedHtml)
-    const [fixedHtml, setFixedHtml] = useState(savedHtml)
-
     useEffect(() => {
-        let updatedHtml = savedHtml.replace(
-            /href="(www\.[^"]*)"/g,
-            'href="https://$1"'
-        )
-        setFixedHtml(updatedHtml)
+        localStorage.setItem("TextEditorContent", JSON.stringify("Write here..."))
+    }, [])
+    // const savedHtml = JSON?.parse(localStorage?.getItem("TextEditorContent") || "")
+    // console.log(savedHtml)
+    // const [fixedHtml, setFixedHtml] = useState(savedHtml)
 
-        const links = document.querySelectorAll(".simple-editor-content a")
-        links.forEach(link => {
-            link.setAttribute("target", "_blank")
-            link.setAttribute("rel", "noopener noreferrer")
-        })
-    }, [savedHtml])
+    // useEffect(() => {
+    //     let updatedHtml = savedHtml.replace(
+    //         /href="(www\.[^"]*)"/g,
+    //         'href="https://$1"'
+    //     )
+    //     setFixedHtml(updatedHtml)
+
+    //     const links = document.querySelectorAll(".simple-editor-content a")
+    //     links.forEach(link => {
+    //         link.setAttribute("target", "_blank")
+    //         link.setAttribute("rel", "noopener noreferrer")
+    //     })
+    // }, [savedHtml])
+
     return (
         <div>
             {/* <RichTextEditor /> */}
             <SimpleEditor />
             {/* <SimpleTextEditor /> */}
-            <div className='tiptap-content' dangerouslySetInnerHTML={{ __html: fixedHtml }} />
+            {/* <div className='tiptap-content' dangerouslySetInnerHTML={{ __html: fixedHtml }} /> */}
         </div>
     )
 }
