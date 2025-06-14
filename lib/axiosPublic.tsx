2f5266cc8e531dbf77b2a196c1e092187b010553
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-06-14 14:40:16 +0600

    refactor(axiosPublic): simplify baseURL assignment for clarity

--- lib/axiosPublic.tsx
+++ lib/axiosPublic.tsx
@@ -1,8 +1,16 @@
 import axios from "axios";
 
+let baseURL = "";
+
+if (typeof window !== "undefined" && window.location.hostname === "localhost") {
+  baseURL = "http://localhost:3000";
+} else {
+  baseURL = "https://skills-hatch.vercel.app"; // Replace with actual domain
+}
+
 export const axiosPublic = axios.create({
-    baseURL: window.location.hostname === "localhost" ? "http://localhost:3000" : "https://skills-hatch.vercel.app",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
