6c036eab9f6f6b1efefecd1eb1f5bbff534ed0d0
DevRedoy404 <devredoy404@gmail.com>
2025-06-15 01:14:05 +0600

    done

---
+++ app/courses/course-details/[id]/page.tsx
@@ -0,0 +1,78 @@
+"use client"
+import { useKindeUser } from '@/hooks/useKindeUser'
+import { axiosPublic } from '@/lib/axiosPublic'
+import { useQuery } from '@tanstack/react-query'
+import { HeartIcon } from 'lucide-react'
+import Link from 'next/link'
+import { useParams, useSearchParams } from 'next/navigation'
+import React, { useState } from 'react'
+import { IoIosArrowBack } from 'react-icons/io'
+import { FaHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
+
+
+export default function CourseDetails() {
+  const [toggleSection, settoggleSection] = useState("about");
+  const params = useParams()
+  const {user} = useKindeUser()
+  const {data:course} = useQuery({
+    queryKey: ['single-course'],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/api/course?kindeId=${user?.id}&courseId=${params?.id}`)
+      // console.log(res.data)
+      return res.data.data
+    },
+    enabled: !!user
+  })
+  console.log("single course", course)
+  const added = false
+  return (
+    <div>
+        <Link href={"/courses"} className='flex items-center gap-3 font-semibold'><IoIosArrowBack />
+        Back</Link>
+        <div className='grid grid-cols-7 gap-5'>
+          {/* text container  */}
+            <div className='bg-background col-span-4 rounded-lg p-5'>
+              <h1 className='font-semibold text-2xl'>{course?.title}</h1>
+              <p className='text-muted-foreground font-semibold py-5'>{course?.description}</p>
+              <div className='flex gap-1 items-center font-semibold text-muted-foreground'>
+                {/* <p>{course?.instructor?.fullName} | </p> */}
+                <p>⭐{course?.ratings} | </p>
+                <p>Review ({course?.reviews?.length}) | </p>
+                <p>{course?.studentsEnrolled?.length} Students</p>
+              </div>
+              <div className='flex py-5 items-center gap-2 font-semibold'>
+                <img className='w-8 rounded-lg' src={course?.instructor?.picture} alt="" />
+                <p>{course?.instructor?.fullName}</p>
+              </div>
+              {/* review and about section  */}
+              <div className='pt-5'>
+                {/* header  */}
+                <div className='flex items-center gap-4 border-b-2 pb-2 pl-1 font-semibold text-muted-foreground relative'>
+                  <p onClick={() => settoggleSection("about")} className={`${toggleSection === "about" && 'text-primary'} cursor-pointer select-none`}>About</p>
+                  <p onClick={() => settoggleSection("review")} className={`${toggleSection === "review" && 'text-primary'} cursor-pointer select-none`}>Review</p>
+                  <div className={`${toggleSection === "about" ? 'left-0' : 'left-16'} w-14 h-0.5 rounded-lg absolute -bottom-0.5  bg-primary`}></div>
+                </div>
+              </div>
+            </div>
+            {/* image container  */}
+            <div className='bg-background col-span-3 rounded-lg p-5'>
+              <div>
+                <img src={course?.thumbnail} alt="" className='rounded-lg'/>
+              </div>
+              <div className='mt-5 flex items-center justify-between'>
+                <p className='font-bold text-2xl'>${course?.price}</p>
+                {!added ? <div className='flex gap-2 items-center cursor-pointer'>
+                  <FaRegHeart className='text-2xl cursor-pointer text-muted-foreground'/>
+                  <p className='text-muted-foreground'>Add to whishlist</p>
+                </div> : <div className='flex gap-2 items-center cursor-pointer'>
+                  <FaHeart className='text-2xl text-red-500 cursor-pointer'/>
+                  <p className='font-semibold text-red-500'>Added</p>
+                </div>}
+                
+              </div>
+            </div>
+        </div>
+    </div>
+  )
+}
